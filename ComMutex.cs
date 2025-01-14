afb4139696bcf0723f6dfa5cef5060a075b3f5a1
kimboslice99 <94807745+kimboslice99@users.noreply.github.com>
2025-01-14 12:20:42 -0500

    Initial commit

---
+++ ComMutex.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace ComMutex
+{
+    [Guid("184B7CD9-CA53-4C9C-B26E-5EF14AC57270")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ComVisible(true)]
+    public class ComMutex
+    {
+        private Mutex mutex;
+
+        /// <summary>
+        /// Creates a named mutex.
+        /// </summary>
+        /// <param name="name">The name of the mutex.</param>
+        /// <returns>True if the mutex was created successfully, false otherwise.</returns>
+        public bool Create(string name)
+        {
+            try
+            {
+                mutex = new Mutex(false, name);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Waits for the mutex to be available indefinitely.
+        /// </summary>
+        /// <returns>True if the mutex was acquired, false otherwise.</returns>
+        public bool Wait()
+        {
+            return WaitWithTimeout(-1);
+        }
+
+        /// <summary>
+        /// Waits for the mutex to be available within a specific timeout.
+        /// </summary>
+        /// <param name="timeoutMilliseconds">The timeout in milliseconds.</param>
+        /// <returns>True if the mutex was acquired, false otherwise.</returns>
+        public bool WaitWithTimeout(int timeoutMilliseconds)
+        {
+            try
+            {
+                if (mutex == null)
+                    return false;
+
+                return mutex.WaitOne(timeoutMilliseconds);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Releases the mutex.
+        /// </summary>
+        /// <returns>True if the mutex was released successfully, false otherwise.</returns>
+        public bool ReleaseMutex()
+        {
+            try
+            {
+                if (mutex == null)
+                    return false;
+
+                mutex.ReleaseMutex();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ComMutex")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ComMutex")]
+[assembly: AssemblyCopyright("Copyright © kimboslice99 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e8f5066f-ef7b-46b2-aaef-ce3f99604a1c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
